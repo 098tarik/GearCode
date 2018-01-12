03c4a8d6c6e59178f2f040df0cba58b539c01b86
Tarik Brown <tarik098@yahoo.com>
2018-01-11 19:44:29 -0500

    Gear Code Untested

--- src/org/usfirst/frc/team86/robot/GearB.java
+++ src/org/usfirst/frc/team86/robot/GearB.java
@@ -2,55 +2,88 @@ package org.usfirst.frc.team86.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Relay;
 
 public class GearB {
 
 	private Solenoid extSol;
 	private Solenoid rotSol;
 	private Solenoid gripSol;
-	private DigitalInput bannarSen;
-	private DigitalInput solSen;
+
 	
-	public GearB(Solenoid extSol, Solenoid rotSol, Solenoid gripSol, DigitalInput bannerSen, DigitalInput solSen ) {
+	public GearB(Solenoid extSol, Solenoid rotSol, Solenoid gripSol) {
 		
 		this.extSol = extSol;
 		this.rotSol = rotSol;
 		this.gripSol = gripSol;
-		this.bannarSen = bannerSen;
-		this.solSen = solSen;
-		
 	}
 	
 	public void gearGrab() {
 		int state = 0;
 		double time = 0;
-		switch(state) {
-
-		case 0:
 		time = System.currentTimeMillis();
+		switch(state){
+
+         
+		// rotate only if not extended 
+		case 0:
+			if(IO.grabButton.equals(true) && IO.solSen.equals(false)) {
 		setGearState(false,false,true);
+		} else if(IO.grabButton.equals(false)) {
+			state = 5;
+		}else {
+			setGearState(false,false,false);
+		}
 		state++;
 		break;
+		
+			
+		// extend if gear is found
 		case 1:
-			setGearState(true,false,true);
+			if(IO.grabButton.equals(true) && IO.bannerSen.equals(true)) {
+				time = System.currentTimeMillis();
+				if(System.currentTimeMillis() > time + 500) {
+					setGearState(true,false,true);
+					state++;
+				}
+			}
 		break;
+		// grip after extended 
 		case 2:
+			time = System.currentTimeMillis();
+			if(IO.solSen.equals(true) && System.currentTimeMillis() > time + 500) {
 			setGearState(true,true,true);
+			state++;
+			}
 			break;
-			
-		case 3:
+			// retract after grip
+		case 3: 
+			time = System.currentTimeMillis();
+			if(System.currentTimeMillis() > time + 500) {
 			setGearState(false,true,true);
+			state++; }
 			break;
-			
+			// rotate after retract 
 		case 4:
+			time = System.currentTimeMillis();
+			if(IO.solSen.equals(false) && System.currentTimeMillis() > time + 500) {
 			setGearState(false,true,false);
+			state++;
+			}
+			break;
+			// release gear 
+		case 5: 
+			time = System.currentTimeMillis();
+			if(IO.releaseButton.equals(true) && System.currentTimeMillis() > time + 500) {
+				setGearState(false,false,false);
+				state = 0;
+			}
 			break;
 			
 			default:
 				
 				setGearState(false,false,false);
-				
-			
+				state = 0;
 		}
 	}
 	
@@ -58,23 +91,28 @@ public class GearB {
 	
 	public void setGearState(boolean ext, boolean grip, boolean rot ) {
 		
-		if(ext) 
+		if(ext == true) 
 			extSol.set(true);
-		else 
+		else if (ext == false)
 			extSol.set(false);
 			
-		if(grip) 
+		if(grip == true) 
 			gripSol.set(true);
-		else
+		else if (rot == false)
 			gripSol.set(false);
 		
-		if(rot) 
+		if(rot == true) 
 			rotSol.set(true);
-		else
+		else if (rot == false)
 			rotSol.set(false);
 			
-			
-		
+	}
+	
+	public void updateCompressor(){
+		if(IO.compressor.enabled())
+			IO.compressorRelay.set(Relay.Value.kForward);
+		else
+			IO.compressorRelay.set(Relay.Value.kOff);
 	}
 }
 
--- src/org/usfirst/frc/team86/robot/IO.java
+++ src/org/usfirst/frc/team86/robot/IO.java
@@ -1,5 +1,25 @@
 package org.usfirst.frc.team86.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 public class IO {
+	
+	public static Solenoid extSol = new Solenoid(1, 0);
+	public static Solenoid rotSol = new Solenoid(1, 1);
+	public static Solenoid gripSol = new Solenoid(1, 2);
+	
+	public static Compressor compressor = new Compressor(1);
+	public static Relay compressorRelay = new Relay(0);
+	
+	public static DigitalInput bannerSen = new DigitalInput(1);
+	public static DigitalInput solSen = new DigitalInput(0);
+	
+	public static Button grabButton = new JoystickButton(coJoystick, 11);
+	public static Button releaseButton = new JoystickButton(coJoystick, 12);
 
 }
